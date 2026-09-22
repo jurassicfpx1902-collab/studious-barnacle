6f235eec2d82b7f7551db158e8ef93f6610cf43d
jurassicfpx1902-collab <jurassicfpx1902@gmail.com>
2026-09-22 10:00:21 -0300

    Initialize AudioSystem and UISystem on load

---
+++ js/main.js
@@ -0,0 +1,16 @@
+"use strict";
+
+window.addEventListener(
+    "DOMContentLoaded",
+    () => {
+
+        AudioSystem.init();
+
+        UISystem.init();
+
+        console.log(
+            "[THE DARK JEWEL] Prototype 0.1 initialized."
+        );
+
+    }
+);
