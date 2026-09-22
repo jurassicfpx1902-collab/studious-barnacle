09b4e3267af6519cfa2440d6efbe215b8fe23a4b
jurassicfpx1902-collab <jurassicfpx1902@gmail.com>
2026-09-22 12:15:49 -0300

    Refactor Player class and update methods
    Refactor Player class to improve structure and functionality.

--- js/player.js
+++ js/player.js
@@ -1,152 +1,315 @@
-export class Player {
+class Player {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
 
-    constructor(collisionSystem) {
+        this.width = 22;
+        this.height = 42;
 
-        this.collisionSystem = collisionSystem;
+        this.speed = 2.2;
 
-        this.x = 100;
-        this.y = 440;
+        this.directionX = 0;
+        this.directionY = 1;
 
-        this.radius = 13;
-
-        this.speed = 145;
-
-        this.directionX = 1;
-        this.directionY = 0;
-
-        this.active = true;
+        this.isMoving = false;
     }
 
-    setDirection(x, y) {
+    update(input) {
+        this.directionX = input.x;
+        this.directionY = input.y;
 
-        const length =
-            Math.hypot(x, y);
-
-        if (length < 0.05) {
-
-            this.directionX = 0;
-            this.directionY = 0;
-
-            return;
-        }
-
-        this.directionX = x / length;
-        this.directionY = y / length;
-    }
-
-    update(deltaTime) {
-
-        if (!this.active) {
-            return;
-        }
-
-        const movementX =
-            this.directionX *
-            this.speed *
-            deltaTime;
-
-        const movementY =
-            this.directionY *
-            this.speed *
-            deltaTime;
-
-        this.collisionSystem.moveEntity(
-            this,
-            movementX,
-            movementY
+        const length = Math.hypot(
+            this.directionX,
+            this.directionY
         );
+
+        if (length > 0) {
+            const normalizedX =
+                this.directionX / length;
+
+            const normalizedY =
+                this.directionY / length;
+
+            this.x += normalizedX * this.speed;
+            this.y += normalizedY * this.speed;
+
+            this.isMoving = true;
+        } else {
+            this.isMoving = false;
+        }
     }
 
-    distanceTo(x, y) {
+    draw(ctx) {
+        ctx.save();
 
-        return Math.hypot(
-            this.x - x,
-            this.y - y
-        );
-    }
-
-    draw(context) {
-
-        context.save();
-
-        context.translate(
+        ctx.translate(
             this.x,
             this.y
         );
 
-        context.rotate(
-            Math.atan2(
-                this.directionY,
-                this.directionX
-            )
+        const moving =
+            this.isMoving ? 1 : 0;
+
+        // Shadow
+        ctx.fillStyle =
+            "rgba(0, 0, 0, 0.35)";
+
+        ctx.beginPath();
+
+        ctx.ellipse(
+            0,
+            21,
+            13,
+            4,
+            0,
+            0,
+            Math.PI * 2
         );
 
-        /*
-         * Stylized human silhouette.
-         */
+        ctx.fill();
 
-        context.fillStyle = "#c7a77b";
+        // Legs
+        ctx.fillStyle = "#15191d";
 
-        context.fillRect(
-            5,
-            -6,
-            9,
-            12
-        );
-
-        context.fillStyle = "#18212a";
-
-        context.fillRect(
-            -8,
-            -10,
-            15,
-            20
-        );
-
-        context.fillRect(
-            -11,
-            -8,
-            5,
+        ctx.fillRect(
+            -9,
+            8,
+            7,
             15
         );
 
-        context.fillRect(
+        ctx.fillRect(
+            2,
+            8,
             7,
+            15
+        );
+
+        // Boots
+        ctx.fillStyle = "#090b0d";
+
+        ctx.fillRect(
+            -10,
+            20,
+            9,
+            5
+        );
+
+        ctx.fillRect(
+            1,
+            20,
+            10,
+            5
+        );
+
+        // Hips
+        ctx.fillStyle = "#20252a";
+
+        ctx.fillRect(
+            -9,
+            4,
+            18,
+            8
+        );
+
+        // Torso
+        ctx.beginPath();
+
+        ctx.moveTo(-8, -11);
+        ctx.lineTo(8, -11);
+        ctx.lineTo(10, 5);
+        ctx.lineTo(-10, 5);
+
+        ctx.closePath();
+
+        ctx.fillStyle = "#11161a";
+        ctx.fill();
+
+        // Vest / equipment
+        ctx.fillStyle = "#252b30";
+
+        ctx.fillRect(
             -7,
-            5,
-            14
+            -7,
+            14,
+            9
         );
 
-        context.fillRect(
+        // Center detail
+        ctx.fillStyle = "#080a0c";
+
+        ctx.fillRect(
+            -2,
             -6,
-            9,
-            5,
-            9
+            4,
+            7
         );
 
-        context.fillRect(
-            3,
-            9,
-            5,
-            9
-        );
+        // Shoulders
+        ctx.fillStyle = "#171c20";
 
-        context.fillStyle = "#0b1015";
+        ctx.beginPath();
 
-        context.fillRect(
+        ctx.arc(
+            -9,
             -8,
-            16,
-            7,
-            3
+            4,
+            0,
+            Math.PI * 2
         );
 
-        context.fillRect(
+        ctx.arc(
+            9,
+            -8,
+            4,
+            0,
+            Math.PI * 2
+        );
+
+        ctx.fill();
+
+        // Arms
+        ctx.fillRect(
+            -12,
+            -5,
+            6,
+            13
+        );
+
+        ctx.fillRect(
+            6,
+            -5,
+            6,
+            13
+        );
+
+        // Hands
+        ctx.fillStyle = "#c7a88c";
+
+        ctx.beginPath();
+
+        ctx.arc(
+            -9,
+            9,
+            2.5,
+            0,
+            Math.PI * 2
+        );
+
+        ctx.arc(
+            9,
+            9,
+            2.5,
+            0,
+            Math.PI * 2
+        );
+
+        ctx.fill();
+
+        // Neck
+        ctx.fillStyle = "#b8957a";
+
+        ctx.fillRect(
+            -3,
+            -14,
+            6,
+            5
+        );
+
+        // Head
+        ctx.fillStyle = "#d0ad90";
+
+        ctx.beginPath();
+
+        ctx.ellipse(
+            0,
+            -19,
+            7,
+            8,
+            0,
+            0,
+            Math.PI * 2
+        );
+
+        ctx.fill();
+
+        // Blonde hair
+        ctx.fillStyle = "#c8ad67";
+
+        ctx.beginPath();
+
+        ctx.arc(
+            0,
+            -22,
+            7,
+            Math.PI,
+            Math.PI * 2
+        );
+
+        ctx.fill();
+
+        ctx.fillRect(
+            -7,
+            -22,
+            14,
+            4
+        );
+
+        // Face direction
+        ctx.fillStyle = "#6e5548";
+
+        const eyeX =
+            this.directionX > 0
+                ? 3
+                : this.directionX < 0
+                    ? -3
+                    : 0;
+
+        ctx.fillRect(
+            eyeX,
+            -19,
+            1.5,
+            1.5
+        );
+
+        // Back equipment
+        ctx.fillStyle = "#0a0d10";
+
+        ctx.fillRect(
+            -11,
+            -7,
             3,
-            16,
-            7,
-            3
+            13
         );
 
-        context.restore();
+        ctx.fillRect(
+            8,
+            -7,
+            3,
+            13
+        );
+
+        // Movement detail
+        if (moving) {
+            ctx.globalAlpha = 0.35;
+
+            ctx.fillStyle = "#3d5663";
+
+            ctx.fillRect(
+                -13,
+                12,
+                2,
+                5
+            );
+
+            ctx.fillRect(
+                11,
+                12,
+                2,
+                5
+            );
+        }
+
+        ctx.restore();
     }
 }
