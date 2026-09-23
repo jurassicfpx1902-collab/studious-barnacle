7e4a6ac79ea0d74aae497943c98e8c8d9dd0f0b7
jurassicfpx1902-collab <jurassicfpx1902@gmail.com>
2026-09-22 21:23:26 -0300

    Refactor GameOverSystem and integrate gameController
    Refactor GameOverSystem to remove constructor and update methods. Integrate gameController for handling game over and retry actions.

--- js/gameover.js
+++ js/gameover.js
@@ -1,43 +1,18 @@
-export class GameOverSystem {
+class GameOverSystem {
+    show() {
+        audioSystem.playGameOver();
+        audioSystem.setIntensity("danger");
 
-    constructor(game) {
-
-        this.game = game;
-
-        this.active = false;
-        this.timer = 0;
-
-        this.duration = 2.8;
+        if (window.gameController) {
+            gameController.showGameOver();
+        }
     }
 
-    start(enemy) {
-
-        if (this.active) {
-            return;
-        }
-
-        this.active = true;
-        this.timer = 0;
-
-        this.game.player.active = false;
-        enemy.stop();
-
-        this.game.audioSystem.playAlert();
-    }
-
-    update(deltaTime) {
-
-        if (!this.active) {
-            return;
-        }
-
-        this.timer += deltaTime;
-
-        if (this.timer >= this.duration) {
-
-            this.active = false;
-
-            this.game.showGameOver();
+    retry() {
+        if (window.gameController) {
+            gameController.startGameplay();
         }
     }
 }
+
+window.gameOverSystem = new GameOverSystem();
